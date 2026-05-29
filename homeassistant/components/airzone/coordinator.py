f5af1b63f36fec49e3c67ab25e0277e524440240
cddu33 <cdibout@gmail.com>
2026-05-29 21:44:07 +0200

    remove scan interval

--- homeassistant/components/airzone/coordinator.py
+++ homeassistant/components/airzone/coordinator.py
@@ -9,11 +9,12 @@ from aioairzone.exceptions import AirzoneError
 from aioairzone.localapi import AirzoneLocalApi
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import AIOAIRZONE_DEVICE_TIMEOUT_SEC, DEFAULT_SCAN_INTERVAL, DOMAIN
+from .const import AIOAIRZONE_DEVICE_TIMEOUT_SEC, DOMAIN
+
+SCAN_INTERVAL = timedelta(seconds=60)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,16 +35,12 @@ class AirzoneUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         """Initialize."""
         self.airzone = airzone
 
-        scan_interval = config_entry.options.get(
-            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-        )
-
         super().__init__(
             hass,
             _LOGGER,
             config_entry=config_entry,
             name=DOMAIN,
-            update_interval=timedelta(seconds=scan_interval),
+            update_interval=SCAN_INTERVAL,
         )
 
     async def _async_update_data(self) -> dict[str, Any]:
